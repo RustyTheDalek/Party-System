5e108fb27bb60a51b9f3abba21d79f3b57c09d43
Ian <enigma994@live.co.uk>
2024-03-19 00:24:05 +0000

    NUI additions -Base HTML/JS/CSS

--- nui/js/index.js
+++ nui/js/index.js
@@ -1,22 +1,44 @@
 
-var playerListContainer 
+var socialWindow
+var playerListContainer
 
-//On DOM load
-$(function () {
-    
-    playerListContainer = $('#player-list tbody');
+window.addEventListener("message", function (event) {
+    console.log(event);
 
-    AddPlayer(1, 'Rusty');
-    AddPlayer(2, 'Someonewithalongname69Someonewithalongname69Someonewithalongname69');
-    AddPlayer(3, 'JC');
-    AddPlayer(4, 'MasterSJT');
-
-    for (let index = 0; index < 50; index++) {
-        AddPlayer(index, 'Rusty:' + index);
+    switch(event.data.action) {
+        case "sendPlayers":
+            console.log('sending players');
+            AddPlayers(event.data.players, event.data.ownSource)
+            break;
+        case "toggleSocial":
+            toggleSocial(event.data.active);
+            break;
     }
 })
 
+//On DOM load
+$(function () {
+    socialWindow = $('#social');
+    playerListContainer = $('#player-list tbody');
+})
+
+function AddPlayers(players, ownSource) {
+
+    console.log(players);
+    console.log(typeof(players));
+
+    if(typeof(players) !== 'object' && Object.keys(players).length <= 0) return;
+    players.forEach(player => {
+        
+        // if(ownSource == player.source) return;
+        
+        AddPlayer(player.source, player.name);
+    });
+}
+
 function AddPlayer(source, name) {
+
+    console.log(`Adding Player ${name} with source ${source}`)
 
     let playerRow = $("<tr/>", {
         id: source
@@ -39,6 +61,8 @@ function AddPlayer(source, name) {
         text: '+'
     });
 
+    addButton.on('click', addPlayertoParty);
+
     playerButtons.append(addButton);
     playerName.append(playerButtons);
 
@@ -46,4 +70,19 @@ function AddPlayer(source, name) {
     playerRow.append(playerName);
 
     playerListContainer.append(playerRow);
+}
+
+function addPlayertoParty() {
+    console.log('Adding player');
+}
+
+function toggleSocial(active) {
+
+    console.log('toggling social');
+
+    if(active) {
+        socialWindow.removeClass('hidden');
+    } else {
+        socialWindow.addClass('hidden');
+    }
 }
