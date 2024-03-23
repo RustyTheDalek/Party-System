f268a4e30012bf0cc32000229b45124bd367e129
Ian <enigma994@live.co.uk>
2024-03-23 12:14:43 +0000

    Added framework for config system -added Shared directory -adjusted fxmanfiset

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,9 +7,9 @@ author 'Rusty'
 version '0.0.1'
 
 client_script 'client.lua'
-server_script 'server.lua'
-shared_script 'shared.lua'
-
+shared_scripts {
+    'shared/*.lua'
+}
 dependency "chat"
 
 ui_page "nui/index.html"
@@ -17,5 +17,6 @@ ui_page "nui/index.html"
 files {
     'nui/index.html',
     'nui/index.css',
-    'nui/js/index.js'
+    'nui/js/index.js',
+    'config.json'
 }
