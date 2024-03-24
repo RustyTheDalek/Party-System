b3538027ea58f40063a45a57d84dde307b976a7e
Ian <enigma994@live.co.uk>
2024-03-24 19:00:56 +0000

    Net evens for removeInvites and joining party

--- client/client.lua
+++ client/client.lua
@@ -11,6 +11,8 @@ function updatePlayerList(players)
     })
 end
 
+TriggerServerEventResource('getPlayers')
+
 AddEventHandler('onClientResourceStart',function(resourceName)
     if(GetCurrentResourceName() ~= resourceName) then return end
 
@@ -29,8 +31,9 @@ end)
 RegisterNetEvent(GetCurrentResourceName() .. ':recievePlayers')
 AddEventHandler(GetCurrentResourceName() .. ':recievePlayers', function(newPlayerList)
     print("received Playerlist")
+    print(dump(newPlayerList))
     playerList = newPlayerList
-    updatePlayerList(playerList)
+    updatePlayerList(newPlayerList)
 end)
 
 RegisterNetEvent(GetCurrentResourceName() .. ':playerJoining')
@@ -86,6 +89,22 @@ RegisterNetEvent(GetCurrentResourceName() .. ':removeFromParty')
 AddEventHandler(GetCurrentResourceName() .. ':removeFromParty', function(sourceToRemove)
     SendNUIMessage({
         action = "removeFromParty",
+        source = sourceToRemove
+    })
+end)
+
+RegisterNetEvent(GetCurrentResourceName() .. ':removeInvite')
+AddEventHandler(GetCurrentResourceName() .. ':removeInvite', function(sourceToRemove)
+    SendNUIMessage({
+        action = "removeInvite",
+        source = sourceToRemove
+    })
+end)
+
+RegisterNetEvent(GetCurrentResourceName() .. ':joinedParty')
+AddEventHandler(GetCurrentResourceName() .. ':joinedParty', function()
+    SendNUIMessage({
+        action = "removeInvite",
         source = sourceToRemove
     })
 end)
