66ff00200fc80de99744676b53df4f5d31bafb5a
Ian <enigma994@live.co.uk>
2024-03-24 01:32:26 +0000

    Back-end functionality for party system

--- server.lua
+++ server.lua
@@ -1,18 +1,16 @@
+local playerList = {}
+local parties = {}
+
 AddEventHandler("playerJoining", function()
     local source = source
 
     print(source .. " is joining")
 end)
 
-local playerList = {}
-
 function GetPlayerList()
     playerList = {}
 
-    print("Getting players")
-
     for _, playerSource in ipairs(GetPlayers()) do
-        
         local name = GetPlayerName(playerSource)
         print("Source: " .. playerSource .. " in-game with name " .. name)
 
@@ -24,7 +22,39 @@ function GetPlayerList()
     end
 end
 
-AddEventHandler('onResourceStart', function (resourceName) 
+function inviteValidate(source, inviteSource)
+
+    if (source == nil or inviteSource == nil) then
+        print("no sources to assess")
+        return false
+    end
+
+    -- if(source == inviteSource) then
+    --     print("Can't accept/reject self!")
+    --     return
+    -- end
+
+    local party = parties[inviteSource]
+
+    if (party == nil) then
+        print("Party doesn't exist")
+        return false
+    end
+
+    if (party.members[source] ~= nil) then
+        print("Already in party, can't accept/reject")
+        return false
+    end
+
+    if (party.invited[source] == nil) then
+        print("Can't accept/reject no invite")
+        return false
+    end
+
+    return true
+end
+
+AddEventHandler('onResourceStart', function(resourceName)
     if (GetCurrentResourceName() ~= resourceName) then
         return
     end
@@ -32,8 +62,83 @@ AddEventHandler('onResourceStart', function (resourceName)
     GetPlayerList()
 end)
 
-RegisterNetEvent('partySystem:getPlayers')
-AddEventHandler('partySystem:getPlayers', function()
+-- #region Network Events -----
+RegisterNetEvent(GetCurrentResourceName() .. ':getPlayers')
+AddEventHandler(GetCurrentResourceName() .. ':getPlayers', function()
     local source = source
-    TriggerClientEvent('partySystem:recievePlayers', source, playerList)
+    TriggerClientEventResource('recievePlayers', source, playerList)
+end)
+
+RegisterNetEvent(GetCurrentResourceName() .. ':invitePlayer')
+AddEventHandler(GetCurrentResourceName() .. ':invitePlayer', function(sourceToInvite)
+    local source = source
+
+    -- if(source == inviteSource) then
+    --     print("Can't invite self!")
+    --     return
+    -- end
+
+    local party = parties[source]
+
+    if (party == nil) then
+        party = Party:New(source)
+    else 
+        if party.members[sourceToInvite] ~= nil then
+            print("already in party, can't invite")
+            return
+        end
+
+        if party.invited[sourceToInvite] ~= nil then
+            print("already invited, can't invite")
+            return
+        end
+    end
+
+    party:InvitePlayer(sourceToInvite, source, GetPlayerName(source))
+    parties[source] = party
+
+end)
+
+RegisterNetEvent(GetCurrentResourceName() .. ':rejectInvite')
+AddEventHandler(GetCurrentResourceName() .. ':rejectInvite', function(sourceToReject)
+    local source = source
+
+    if (not inviteValidate(source, sourceToReject)) then
+        return
+    end
+
+    parties[sourceToReject]:RejectInvite(source, sourceToReject)
+
+end)
+
+RegisterNetEvent(GetCurrentResourceName() .. ':acceptInvite')
+AddEventHandler(GetCurrentResourceName() .. ':acceptInvite', function(sourceToAccept)
+    local source = source
+
+    if (not inviteValidate(source, sourceToAccept)) then
+        return
+    end
+
+    parties[sourceToAccept]:AcceptInvite(source, sourceToAccept)
+
+end)
+
+-- #endregion
+
+function Update()
+    for index, party in pairs(parties) do
+        party:Update()
+        if (party.empty) then
+            print("party empty, removing")
+            party:Close()
+            table.remove(parties, index)
+        end
+    end
+end
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        Update()
+    end
 end)
